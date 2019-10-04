0ced7b90408cc0a5ae1deebe06d36505926545dd
borgogelli <borgogelli@iubar.it>
2019-10-04 19:56:05 +0200

    source formatted

--- App.js
+++ App.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import ProductSlider1 from './ProductSlider1';
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import ProductSlider1 from "./ProductSlider1";
 
-const { width, height } = Dimensions.get('window');
-
+const { width, height } = Dimensions.get("window");
 
 export default function App() {
-
   let images = [
-    {title: "a1", uri: "https://github.com/leecade/react-native-swiper/blob/master/examples/components/Swiper/img/1.jpg?raw=true"},
-    {title: "a2", uri: "https://github.com/leecade/react-native-swiper/blob/master/examples/components/Swiper/img/2.jpg?raw=true"},
-    {title: "a3", uri: "https://github.com/leecade/react-native-swiper/blob/master/examples/components/Swiper/img/3.jpg?raw=true"}
-  ]
+    {
+      title: "a1",
+      uri:
+        "https://github.com/leecade/react-native-swiper/blob/master/examples/components/Swiper/img/1.jpg?raw=true"
+    },
+    {
+      title: "a2",
+      uri:
+        "https://github.com/leecade/react-native-swiper/blob/master/examples/components/Swiper/img/2.jpg?raw=true"
+    },
+    {
+      title: "a3",
+      uri:
+        "https://github.com/leecade/react-native-swiper/blob/master/examples/components/Swiper/img/3.jpg?raw=true"
+    }
+  ];
   return (
     <View style={styles.container}>
-            <View style={styles.slider}>     
-        <ProductSlider1
- 
- 
-        images={images}
-      
-      />  
-          </View>    
+      <View style={styles.slider}>
+        <ProductSlider1 images={images} />
+      </View>
       <Text>Hello World!</Text>
     </View>
   );
@@ -30,7 +35,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   },
   slider: {
     flex: 1,
@@ -40,6 +45,6 @@ const styles = StyleSheet.create({
     marginRight: 0,
     marginLeft: 0,
     marginTop: 0,
-    marginBottom: 10,
-  },  
+    marginBottom: 10
+  }
 });
